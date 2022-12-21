039edbd539666f0a1ae36fdfef253b0e2790ffe3
sharukh <mohammedsharukh987@gmail.com>
2022-12-21 18:59:36 +0530

    monsters rolodex first commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./search-box.styles.css";
+export class SearchBox extends Component {
+  render() {
+    return (
+      <input
+        className={`search-box ${this.props.className}`}
+        type="search"
+        placeholder={this.props.placeholder}
+        onChange={this.props.onChangeHandler}
+      />
+    );
+  }
+}
+
+export default SearchBox;
